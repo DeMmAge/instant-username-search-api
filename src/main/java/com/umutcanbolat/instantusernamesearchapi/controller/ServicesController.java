190c82036031bf383e5c7ccf0fccd84c7d9146f3
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2021-01-19 18:27:46 +0100

    feat: 🎸 remove getAll from services path
    BREAKING CHANGE: 🧨 changes /services/getAll path as /services

--- src/main/java/com/umutcanbolat/instantusernamesearchapi/controller/ServicesController.java
+++ src/main/java/com/umutcanbolat/instantusernamesearchapi/controller/ServicesController.java
@@ -2,6 +2,8 @@ package com.umutcanbolat.instantusernamesearchapi.controller;
 
 import com.umutcanbolat.instantusernamesearchapi.model.ServiceModel;
 import com.umutcanbolat.instantusernamesearchapi.service.CheckService;
+import lombok.Value;
+import lombok.experimental.NonFinal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Value
+@NonFinal
 @RestController
 @RequestMapping("/services")
 public class ServicesController {
-  @Autowired private CheckService checkService;
+  @Autowired CheckService checkService;
 
-  @RequestMapping("/getAll")
+  @RequestMapping
   @Cacheable("services")
   public List<ServiceModel> getServicesList() {
     return checkService.getServices();
